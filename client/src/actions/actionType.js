39314c971d404e7a60e80793fd4316c54017fdcc
William Vuong <wpvuong@ucsd.edu>
2018-10-02 13:40:03 -0700

    Redux Stuff Pt1

---
+++ client/src/actions/actionType.js
@@ -0,0 +1,6 @@
+export const NEW_POST = 'NEW_POST';
+export const DELETE_POST = 'DELETE_POST';
+export const NEW_PROFILE = 'NEW_PROFILE';
+export const LOGIN_REQUEST = 'LOGIN_REQUEST';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAILURE = 'LOGIN_FAILURE';
---
+++ client/src/reducers/postReducers.js
@@ -0,0 +1,4 @@
+import * as types from "../actions/actionTypes";
+
+/* TODO: INTIAL STATE NEEDS TO BE DEFINED  */
+const initialState 
