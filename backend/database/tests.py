47c525c29852b1390f9525fe188af5702797de42
willievuong <willievuong@gmail.com>
2019-12-06 20:12:10 -0800

    testing travis for python

--- backend/database/tests.py
+++ backend/database/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+import datetime
+from .models import *
+from rest_framework.test import APIClient
+from rest_framework import status 
+from django.urls import reverse
+from encryption.encrypt import decrypt
+import random
+import json 
 
-# Create your tests here.
+class ModelTestCase(TestCase):
+    # This class defines the test suite for all models
+
+    def setUp(self):
+        #Define test client and other variables 
+        pass
+    
+    #Need to define create test case 
+    def test_model_create_profile(self): 
+        pass
